cbc821fd6a5b78ae0fa674f9089395d556603d58
Remi Fusade <remi.fusade@gmail.com>
2022-08-03 21:54:42 +0200

    Squishy squish
    Signed-off-by: Remi Fusade <remi.fusade@gmail.com>

--- SpringCharacterController_Unity project/Assets/Scripts/CharacterController.cs
+++ SpringCharacterController_Unity project/Assets/Scripts/CharacterController.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    private Rigidbody playerRb;
-    private CapsuleCollider playerCollider;
+    [Header("References")]
+    public Renderer playerRenderer;
 
+    [Header("Gravity Force")]
     public float gravityForce = 9.8f;
+    public float floorDistanceWhereGravityStartsToSlowDown = 1.0f;
+    public AnimationCurve gravityCurve;
+
+    [Header("Ride height spring")]
+    public float floorDistanceWhereSpringForceIsMaximal = 0.02f;
+    public float maxSpringForce = 100;
+    public AnimationCurve springCurve;
+
+    [Header("Character controller settings")]
     public float jumpForce = 10f;
-
-    [Space]
-    public float moveSpeed = 5;
-
+    public float moveSpeed = 5f;
+    public float targetRideHeight = 0.1f;
     [Space]
     public float groundCheckDistance = 2.0f;
-    public float rideHeight = 0.7f;
-    public float maxSpringForce = 100;
 
-    public AnimationCurve curve;
+    private Rigidbody playerRb;
+    private CapsuleCollider playerCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +37,7 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Jump"))
         {
             playerRb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
         }
@@ -38,57 +45,81 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Distance to the ground
-        float distanceToTheGround = GetDistanceFromTheGround();
-        if (distanceToTheGround < -0.99f)
-        {
-            // No ground nowhere, standard gravity
-            ApplyGravityWithFactor(1.0f);
-        }
-        else
+        if (GetGroundInfo(out float distanceToTheGround))
         {
             // Gravity
             float gravityFactor = 1.0f; // gravity factor goes from 0 to 1 when character is above rideHeight
-            if (distanceToTheGround <= rideHeight)
+            if (distanceToTheGround <= targetRideHeight)
             {
                 gravityFactor = 0;
             }
-            else if (distanceToTheGround <= 2 * rideHeight)
+            else if (distanceToTheGround <= floorDistanceWhereGravityStartsToSlowDown)
             {
-                gravityFactor = (distanceToTheGround - rideHeight) / rideHeight;
+                gravityFactor = (distanceToTheGround - targetRideHeight) / (floorDistanceWhereGravityStartsToSlowDown - targetRideHeight);
             }
             gravityFactor = Mathf.Clamp(gravityFactor, 0, 1);
-            gravityFactor = curve.Evaluate(gravityFactor);
+            gravityFactor = gravityCurve.Evaluate(gravityFactor);
             ApplyGravityWithFactor(gravityFactor);
 
             // "Spring" to keep character above the ground
             float springFactor = 0.0f; // spring factor is 1 when character touches the ground, and is zero when character is exactly at rideHeight
-            if (distanceToTheGround < rideHeight)
+            if (distanceToTheGround <= floorDistanceWhereSpringForceIsMaximal)
             {
-                springFactor = (rideHeight - distanceToTheGround) / rideHeight;
+                springFactor = 1.0f;
+            }
+            else if (distanceToTheGround < targetRideHeight)
+            {
+                springFactor = (targetRideHeight - distanceToTheGround) / (targetRideHeight - floorDistanceWhereSpringForceIsMaximal);
             }
             springFactor = Mathf.Clamp(springFactor, 0, 1);
-            springFactor = curve.Evaluate(springFactor);
+            springFactor = springCurve.Evaluate(springFactor);
             ApplySpringForceWithFactor(springFactor);
+        }
+        else
+        {
+            // No ground nowhere, standard gravity
+            ApplyGravityWithFactor(1.0f);
+
         }
 
         // Horizontal movement
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");        
+        float verticalInput = Input.GetAxis("Vertical");
         playerRb.AddForce(moveSpeed * horizontalInput * Vector3.right);
         playerRb.AddForce(moveSpeed * verticalInput * Vector3.forward);
+        playerRenderer.transform.localRotation = Quaternion.Euler(20*verticalInput, 0, -20*horizontalInput);
     }
-    
-    private float GetDistanceFromTheGround()
+
+    private void LateUpdate()
     {
+        if (GetGroundInfo(out float distanceToGround))
+        {
+            float distanceToGroundFactor = distanceToGround / 3.0f;
+            float yScale = 1 + (distanceToGroundFactor - targetRideHeight);
+            yScale = Mathf.Clamp(yScale, 0.6f, 1.4f);
+            float horizontalScale = 1 - (yScale - 1);
+            playerRenderer.transform.localScale = new Vector3(horizontalScale, yScale, horizontalScale);
+        }
+        else
+        {
+            playerRenderer.transform.localScale = Vector3.one;
+        }
+    }
+
+    private bool GetGroundInfo(out float distance)
+    {
+        bool isGround = false;
+        distance = 0;
         float distanceToTheGround = -1; // no ground
-        if (Physics.SphereCast(this.transform.position, 0.1f, -groundCheckDistance * Vector3.up, out RaycastHit hit))
+        float sphereRadius = playerCollider.radius;
+        if (Physics.SphereCast(this.transform.position, sphereRadius, -groundCheckDistance * Vector3.up, out RaycastHit hit))
         {
             // found the ground!
-            distanceToTheGround = hit.distance - (playerCollider.height / 2.0f);
+            distance = hit.distance - (playerCollider.height / 2.0f);
             DebugMessageWithTimestamp("Raycast hit with distance to the ground = " + distanceToTheGround);
+            isGround = true;
         }
-        return distanceToTheGround;
+        return isGround;
     }
 
     private void ApplyGravityWithFactor(float gravityFactor)
